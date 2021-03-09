d321ec20cf2f5d66f8172fc1507ab8a2c0ea8325
Toni <agomezj@cifpfbmoll.eu>
2021-03-09 12:50:22 +0100

    Spring and Car class

---
+++ src/main/java/com/toni/programming/data/Car.java
@@ -0,0 +1,50 @@
+package com.toni.programming.data;
+
+public class Car {
+
+    private Integer wheels;
+    private Integer enginePower;
+    private String Color;
+    private String model;
+    private String name;
+
+    public Integer getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(Integer wheels) {
+        this.wheels = wheels;
+    }
+
+    public Integer getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(Integer enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
